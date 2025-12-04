610f8ff28c274d58688c88e60a4adc162da19a02
Elliott Amrouche <elliottamrouche27@gmail.com>
2025-12-04 10:45:00 +0100

    textwindows utilisation

--- main/src/main/java/com/jad/main/Main.java
+++ main/src/main/java/com/jad/main/Main.java
@@ -1,10 +1,13 @@
 package com.jad.main;
 
 import com.jad.controller.Controller;
+import com.jad.textwindow.TextWindow;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("KEO's SuperVeloceJota");
+        TextWindow textWindow = new TextWindow();
+        textWindow.setVisible(true);
+        textWindow.display("keo's SuperVeloceJota");
         Controller controller = new Controller();
     }
 }
